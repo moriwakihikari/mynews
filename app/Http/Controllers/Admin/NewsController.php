64c6ea5896fd699dc081672a98d121cc3faf2fdd
moriwakihikari <daiichidaimandaikichi.gi@gmail.com>
2020-01-31 15:40:26 +0000

    first commit

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
 use App\News;
 
 class NewsController extends Controller
@@ -37,5 +36,18 @@ class NewsController extends Controller
       $news->save();
       
       return redirect('admin/news/create');
+  }
+  
+  public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          //検索されたら検索結果を取得する
+          $posts = News::where('title', $cond_title)->get();
+      } else {
+          //それ以外は全てのニュースを取得する
+          $posts = News::all();
+      }
+      return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
   }
 }
